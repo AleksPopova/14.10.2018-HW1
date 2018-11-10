fd9cc686f5ad645e19ee3640ec24b1f4288ce699
AleksPopova <44133562+AleksPopova@users.noreply.github.com>
2018-11-10 22:06:31 +0300

    Create 1.cpp

---
+++ 11.11.18-HW3-Prime/1.cpp
@@ -0,0 +1,22 @@
+/*
+Напишите программу, вычисляющую сумму нечетных цифр целого числа.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   int n, m, t = 0;
+   cin » n;
+   m = (n % 10);
+   while (n >= 1)
+    {
+       if ((m% 2) == 1)
+        {
+           t += m;
+        };
+       n /= 10;
+       m= (n % 10);
+    };
+   cout « t;
+}
